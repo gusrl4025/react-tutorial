b24ae8f7f04bb7ebbde6c1fa28c3640bffcbd122
dizzi <gusrl4025@gmail.com>
2022-07-08 13:30:20 +0900

    1-13 배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 //export한 Hello 컴포넌트를 불러온다.
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
     {
         id: 1,
         username: 'gusrl4025',
@@ -23,9 +36,33 @@ function App() {
         username: '김현기',
         email: '김현기@gmail.com'
     },
-  ];
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  }
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
