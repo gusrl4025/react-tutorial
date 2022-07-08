4999bacc3b46443623c233dc54d323ec3e1a720e
dizzi <gusrl4025@gmail.com>
2022-07-08 10:08:55 +0900

    Velopert's Moder React 1-1 ~ 1-10

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+//리액트 컴포넌트를 만들 땐 리액트를 불러와야 한다
+import React from 'react';
+
+function Hello({color, name, isSpecial }) {
+    return <div style={{ color }}>
+        { isSpecial && <b>*</b> }
+        안녕하세요 {name}
+    </div>
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
+// Hello라는 컴포넌트를 내보내겠다는 의미, 다른 컴포넌트에서 불러올 수 있게 됨
+export default Hello;
