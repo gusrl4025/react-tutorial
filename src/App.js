ef92331e956673026e82f0600e9e928a93d8d179
dizzi <gusrl4025@gmail.com>
2022-07-12 15:15:21 +0900

    1-23 Immer를 사용한 더 쉬운 불변성 관리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useReducer } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -8,10 +9,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: ''
-  },
   users: [
     {
       id: 1,
@@ -34,25 +31,46 @@ const initialState = {
   ]
 }
 
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'CREATE_USER':
+//       return {
+//         users: state.users.concat(action.user)
+//       };
+//     case 'TOGGLE_USER':
+//       return {
+//         ...state,
+//         users: state.users.map(user =>
+//           user.id === action.id ? { ...user, active: !user.active } : user
+//         )
+//       };
+//     case 'REMOVE_USER':
+//       return {
+//         ...state,
+//         users: state.users.filter(user => user.id !== action.id)
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// Immer를 사용한 방법
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      })
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      })
     default:
       return state;
   }
