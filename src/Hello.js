60b76cdbc774f3e157ca3a658248f56d263b9ccc
dizzi <gusrl4025@gmail.com>
2022-07-12 16:45:31 +0900

    1-24 클래스형 컴포넌트

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,19 @@
 //리액트 컴포넌트를 만들 땐 리액트를 불러와야 한다
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({color, name, isSpecial }) {
-    return <div style={{ color }}>
-        { isSpecial && <b>*</b> }
-        안녕하세요 {name}
-    </div>
-}
-
-Hello.defaultProps = {
-    name: '이름없음'
+class Hello extends Component {
+    static defaultProps = {
+        name: '이름없음'
+    };
+    render() {
+        const { color, name, isSpecial } = this.props;
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
 }
 
 // Hello라는 컴포넌트를 내보내겠다는 의미, 다른 컴포넌트에서 불러올 수 있게 됨
